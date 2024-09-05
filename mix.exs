80a1fecee4673f2eb27b552de3dee175efd7a347
Adam Magnusson <admag2001@gmail.com>
2024-09-05 13:42:13 +0200

    Fixed bug where the name input would not accept numbers

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Speakerlist.MixProject do
   def project do
     [
       app: :speakerlist,
-      version: "1.1.5",
+      version: "1.1.6",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
